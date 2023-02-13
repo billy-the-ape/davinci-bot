56a7c7689b1beb2eadc806eba93c0760d9c4d4e7
billytheape.eth <cryptobilly37@gmail.com>
2023-02-12 18:59:26 -0700

    Add some log

--- src/bot/ready.ts
+++ src/bot/ready.ts
@@ -5,6 +5,9 @@ export const ready = (client: Client) => {
   client.on('messageCreate', messageHandler(client));
 
   client.on('ready', () => {
-    console.info(`Bot ready!`);
+    console.info(`Bot ready!`, {
+      model: process.env.OPENAI_MODEL,
+      behavior: process.env.OPENAI_BEHAVIOR,
+    });
   });
 };
